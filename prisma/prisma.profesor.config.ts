058416b777fd3ca7fc2601c2d35f110109189800
Dark <djcabrera@sudamericano.edu.ec>
2025-12-09 03:02:53 -0500

    Commit #02

---
+++ prisma/prisma.profesor.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'esquema-profesor.prisma',
+  migrations: {
+    path: 'migrations-profesor',
+  },
+  datasource: {
+    url: env('DATABASE_URL_PROFESOR'),
+  },
+})
---
+++ prisma/prisma.usuario.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  // Estamos en la carpeta prisma/, así que basta con el nombre:
+  schema: 'esquema-usuario.prisma',
+  migrations: {
+    path: 'migrations-usuario',
+  },
+  datasource: {
+    url: env('DATABASE_URL_USUARIO'),
+  },
+})
